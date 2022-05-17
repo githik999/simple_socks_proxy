3fa2f4de44af26efd3a78d7b9ba7ca88a38a39c0
chris <kakaxi77749@gmail.com>
2022-05-17 22:55:47 +0800

    1863 msn port

--- gate.js
+++ gate.js
@@ -3,7 +3,7 @@ const client = require('./client')
 
 const server = net.createServer((socket)=>{
     new client(socket)
-}).listen(1812)
+}).listen(1863)
 
 server.on('listening',()=>{
     console.log('server listening on',server.address())
