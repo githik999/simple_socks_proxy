a34dfca16f4fe98fab69e718596474209b3e8f9a
chris <kakaxi77749@gmail.com>
2022-05-17 22:53:00 +0800

    port 1812 radis

--- gate.js
+++ gate.js
@@ -3,7 +3,7 @@ const client = require('./client')
 
 const server = net.createServer((socket)=>{
     new client(socket)
-}).listen(1080)
+}).listen(1812)
 
 server.on('listening',()=>{
     console.log('server listening on',server.address())
