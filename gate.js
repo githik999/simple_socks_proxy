10cf8468a5ab0c4a34088c0c001099e6a026f2bf
chris <kakaxi77749@gmail.com>
2022-05-17 22:40:26 +0800

    listening on

--- gate.js
+++ gate.js
@@ -4,3 +4,7 @@ const client = require('./client')
 const server = net.createServer((socket)=>{
     new client(socket)
 }).listen(1080)
+
+server.on('listening',()=>{
+    console.log('server listening on',server.address())
+})
